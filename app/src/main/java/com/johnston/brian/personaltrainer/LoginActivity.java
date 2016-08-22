388045b73d210c4c2e6286eed34cfddc8e9af993
Brian Johnston <brian86j@yahoo.com>
2016-08-22 13:06:02 -0400

    Initial commit

---
+++ app/src/main/java/com/johnston/brian/personaltrainer/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.johnston.brian.personaltrainer;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
