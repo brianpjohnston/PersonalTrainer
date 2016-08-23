ceb1ed8206b2de589c1b163a22885ec08fb804c0
Brian Johnston <brian86j@yahoo.com>
2016-08-23 12:51:03 -0400

    added toast for bad username/password

--- app/src/main/java/com/johnston/brian/personaltrainer/LoginActivity.java
+++ app/src/main/java/com/johnston/brian/personaltrainer/LoginActivity.java
@@ -1,14 +1,21 @@
 package com.johnston.brian.personaltrainer;
 
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RectShape;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText mUser;
+    private EditText mPassword;
+    private Button mLogin;
+
+
 
 
 
@@ -16,6 +23,40 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+        mUser = (EditText)findViewById(R.id.text_user);
+        mPassword= (EditText)findViewById(R.id.text_password);
+        mLogin = (Button) findViewById(R.id.button_login);
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                if (mUser.getText().toString().equals("jdoe")) {
+                    if(mPassword.getText().toString().equals("welcome1"))
+                        {
+                            //// TODO: 8/23/2016
+
+                        }
+                    else{
+
+
+
+
+
+                       Toast.makeText(getApplicationContext(), "Bad username/password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+
+
+                    Toast.makeText(getApplicationContext(), "Bad username/password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
 
